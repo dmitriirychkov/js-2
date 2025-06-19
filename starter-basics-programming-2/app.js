edec0eef5a03afd5e6008f990956e4562a7e7098
dmi <118729000+dmitriirychkov@users.noreply.github.com>
2025-06-19 12:26:17 +0500

    Create app.js

---
+++ starter-basics-programming-2/app.js
@@ -0,0 +1,38 @@
+// Создание переменных
+let a = 15;
+let b = 25;
+let c = 35;
+let d = 45;
+
+// Математические операции
+let result1 = a + b;    
+let result2 = d / a;    
+let result3 = c * b;    
+let result4 = b - b;    
+let result5 = d - c;    
+
+// Операции сравнения
+let comparison1 = a < b;    
+let comparison2 = a < d;    
+let comparison3 = a > b;    
+let comparison4 = c >= c;   
+let comparison5 = b == d;  
+let comparison6 = d == d;   
+let comparison7 = c != a;   
+
+// Вывод результатов в консоль
+console.log("Результаты математических операций:");
+console.log("a + b =", result1);
+console.log("d / a =", result2);
+console.log("c * b =", result3);
+console.log("b - b =", result4);
+console.log("d - c =", result5);
+
+console.log("\nРезультаты сравнений:");
+console.log("a < b:", comparison1);
+console.log("a < d:", comparison2);
+console.log("a > b:", comparison3);
+console.log("c >= c:", comparison4);
+console.log("b == d:", comparison5);
+console.log("d == d:", comparison6);
+console.log("c != a:", comparison7);
