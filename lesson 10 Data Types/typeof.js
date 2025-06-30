8ee2f83c4c9bb420b977bec9ac8e1c5cafe1e69a
dmi <118729000+dmitriirychkov@users.noreply.github.com>
2025-06-30 21:02:31 +0500

    Create typeof.js

---
+++ lesson 10 Data Types/typeof.js
@@ -0,0 +1,11 @@
+console.log(typeof 'Dmitrii');
+console.log(typeof 5);
+console.log(typeof false);
+console.log(typeof Symbol('id'));
+console.log(typeof undefined);
+console.log(typeof 100n);
+console.log(typeof console);
+
+// Исключения 
+console.log(typeof null);
+console.log(typeof console.log);
