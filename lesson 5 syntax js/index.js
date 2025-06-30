25259449a0e746a1ade231cadf1b0d6339c4a84f
dmi <118729000+dmitriirychkov@users.noreply.github.com>
2025-06-30 16:47:23 +0500

    Create index.js

---
+++ lesson 5 syntax js/index.js
@@ -0,0 +1,11 @@
+// var, let, const
+var text = 'Hello World';
+let name = 'Dmitrii';
+const programmingLanguage = 'JavaScript';
+
+name = 'S0er';
+text = 'Hello';
+
+console.log(name);
+console.log(text);
+console.log(programmingLanguage);
