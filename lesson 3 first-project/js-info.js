d3163e9d42a68b7426fe7a09b4a9988cf2fc7453
dmi <118729000+dmitriirychkov@users.noreply.github.com>
2025-06-30 15:45:12 +0500

    Create js-info.js

---
+++ lesson 3 first-project/js-info.js
@@ -0,0 +1,9 @@
+alert("Язык программирования JavaScript был разработан Бренданом Эйхом.");
+
+alert("JavaScript был разработан за 10 дней.");
+
+alert("JavaScript появился в 1995 году.");
+
+alert("JavaScript — это не Java.");
+
+alert("Спецификация JavaScript называется ECMAScript.");
