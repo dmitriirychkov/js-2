929419e2bb889fb4b40cb8a862c6549b54ee0058
dmi <118729000+dmitriirychkov@users.noreply.github.com>
2025-06-30 18:24:01 +0500

    Update index.js

--- lesson 6/index.js
+++ lesson 6/index.js
@@ -1,1 +1,18 @@
 // JavaScript-код с созданием переменных
+
+/* Автор:
+Дмитрий Рычков */
+
+// 1. Создание переменных
+let myName = "Дмитрий";
+let mySurname = "Рычков";
+const myFavoriteDrink = "Черный чай";
+const myFavoriteAnimal = "-";
+let myFavoriteProgrammingLanguage = "JavaScript";
+
+// 2. Вывод переменных в консоль
+console.log("Моё имя:", myName);
+console.log("Моя фамилия:", mySurname);
+console.log("Мой любимый напиток:", myFavoriteDrink);
+console.log("Моё любимое животное:", myFavoriteAnimal);
+console.log("Мой любимый язык программирования:", myFavoriteProgrammingLanguage);
