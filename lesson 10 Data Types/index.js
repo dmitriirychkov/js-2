c733145f27408f629ee76be896eb62b5e6800f50
dmi <118729000+dmitriirychkov@users.noreply.github.com>
2025-06-30 20:38:29 +0500

    Create index.js

---
+++ lesson 10 Data Types/index.js
@@ -0,0 +1,35 @@
+// 1. string
+const favoriteDink = 'black tea';
+console.log(favoriteDink);
+
+// 2. number
+const numberOfMugs = 3;
+console.log(numberOfMugs);
+
+// 3. boolean
+const isColdDrink =true;
+console.log(isColdDrink);
+
+// 4. null
+const studentFavoriteDrink = null;
+console.log(studentFavoriteDrink);
+
+// 5. undefined
+let carOwner = undefined;
+console.log(carOwner);
+
+// 6. object
+const drink = {
+    favoriteDink: 'Tea',
+    numberOfMugs: 3,
+    isColdDrink: true,
+};
+console.log(drink);
+
+// 7. symbol
+const id = Symbol('id');
+console.log(id);
+
+// 8. bigint
+const bigIntNumber = BigInt(10);
+console.log(bigIntNumber);
