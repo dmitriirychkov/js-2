b5a2e6048336cc6ac05595d656bc8d2c240787aa
dmi <118729000+dmitriirychkov@users.noreply.github.com>
2025-07-04 10:23:06 +0500

    Create app.js

---
+++ lesson 19/app.js
@@ -0,0 +1,9 @@
+const myName = 'Ваше Имя';
+const programmingLanguage = 'JavaScript';
+const courseCreatorName = 'Дмитрий';
+const reasonText = 'мне нравится создавать интерактивные веб-приложения';
+const numberOfMonth = 1;
+
+const result = `Всем привет! Меня зовут ${myName}. Сейчас я изучаю язык программирования ${programmingLanguage} на курсе по ${programmingLanguage} у ${courseCreatorName}. Я хочу стать веб-разработчиком, потому что ${reasonText}. До этого я изучал(а) ${programmingLanguage} ${numberOfMonth} месяцев(а). Я уверен(а), что пройду данный курс до конца!`;
+
+console.log(result);
