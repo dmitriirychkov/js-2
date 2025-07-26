c2b4238ddbfca42bbefe16369095870849c03789
dmi <118729000+dmitriirychkov@users.noreply.github.com>
2025-07-26 12:53:02 +0500

    Update index.js

--- lesson 36 - Подсчёт количества делителей числа/index.js
+++ lesson 36 - Подсчёт количества делителей числа/index.js
@@ -23,11 +23,22 @@ function getDivisorsCount(number) {
     let count = 2; // Всегда есть делители 1 и само число
     
     // Проверяем делители от 2 до половины числа
-    for (let i = 2; i <= number / 2; i++) {
+    for (let i = 2; i <= Math.sqrt(number); i++) {
         if (number % i === 0) {
-            count++;
+            // Если делитель - квадратный корень, учитываем его один раз
+            if (i * i === number) {
+                count++;
+            } else {
+                count += 2; // Учитываем и i, и number/i
+            }
         }
     }
     
     return count;
 }
+
+// Примеры использования из задания
+console.log(getDivisorsCount(4));  // Вернет 3 (делители - 1, 2, 4)
+console.log(getDivisorsCount(5));  // Вернет 2 (делители - 1, 5)
+console.log(getDivisorsCount(12)); // Вернет 6 (делители - 1, 2, 3, 4, 6, 12)
+console.log(getDivisorsCount(30)); // Вернет 8 (делители - 1, 2, 3, 5, 6, 10, 15, 30)
