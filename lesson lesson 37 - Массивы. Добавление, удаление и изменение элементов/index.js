c671c3b339f28ded47cf9e0cd772fbdc7ad2acbc
dmi <118729000+dmitriirychkov@users.noreply.github.com>
2025-07-26 13:36:40 +0500

    Create index.js

---
+++ lesson lesson 37 - Массивы. Добавление, удаление и изменение элементов/index.js
@@ -0,0 +1,7 @@
+const salariesOfDevelopers = [400, 500, 600, 2000, 350];
+console.log('salariesOfDevelopers', salariesOfDevelopers);
+
+console.log(salariesOfDevelopers.length);
+
+const names = ['Dmitrii', 'Artur', 'Nikita'];
+console.log('names', names);
