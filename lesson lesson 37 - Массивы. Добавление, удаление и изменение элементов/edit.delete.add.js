fda4e678d80f482fe4443fa1be4c05c451020540
dmi <118729000+dmitriirychkov@users.noreply.github.com>
2025-07-26 14:18:39 +0500

    Create edit.delete.add.js

---
+++ lesson lesson 37 - Массивы. Добавление, удаление и изменение элементов/edit.delete.add.js
@@ -0,0 +1,8 @@
+const salariesOfDevelopers = [400, 500, 600, 2000, 350];
+
+// Добавление элементов 
+const newSeniorDeveloperSalary = 5000;
+salariesOfDevelopers.push(newSeniorDeveloperSalary);
+
+salariesOfDevelopers.unshift(100);
+console.log('salariesOfDevelopers', salariesOfDevelopers);
